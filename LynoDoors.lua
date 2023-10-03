bfd087348ef58cb13eafb49ae4147145b6d37228
Lunix & Lyno <142291970+LynoForWindows@users.noreply.github.com>
2023-10-03 09:07:22 +0800

    Create LynoDoors.lua
    Achievements Custom

---
+++ LynoDoors.lua
@@ -0,0 +1,8 @@
+local Achievements = loadstring(game:HttpGet("https://raw.githubusercontent.com/RegularVynixu/Utilities/main/Doors/Custom%20Achievements/Source.lua"))()
+
+Achievements.Get({
+    Title = "Thanks For Using Lyno!",
+    Desc = "You Used Lyno Script",
+    Reason = "You Supported Lyno Daddy !",
+    Image = "https://cdn.discordapp.com/icons/1126672205225394328/7ed357cbdbe9f4aa608e497541844695.png?size=1024",
+})
