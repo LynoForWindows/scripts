cd75ff684c41eb0db85f0ef9710bdbe6bf33d95a
LynoConfigs <142291970+LynoForWindows@users.noreply.github.com>
2023-10-13 22:28:02 +0800

    TradeScamez
    nice!

---
+++ PSX/TradeScam.lua
@@ -0,0 +1,8 @@
+local OrionLib = loadstring(game:HttpGet(('https://raw.githubusercontent.com/shlexware/Orion/main/source')))()
+OrionLib:MakeNotification({
+	Name = "Hey !",
+	Content = "Join https://discord.gg/cv4YsrY2b2 for script !",
+	Image = "rbxassetid://4483345998",
+	Time = 500
+})
+setclipboard("https://discord.gg/cv4YsrY2b2")
