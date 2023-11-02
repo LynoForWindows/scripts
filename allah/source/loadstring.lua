c9d40002e928676990987ed9f4986bfe32ed46ff
LynoConfigs <142291970+LynoForWindows@users.noreply.github.com>
2023-11-02 19:41:37 +0800

    Update loadstring.lua

--- allah/source/loadstring.lua
+++ allah/source/loadstring.lua
@@ -1,1 +1,9 @@
+--Skid my code 😭😱
+--[[
+Ideas : xylexvxpe/stxrlyno
+main dev : xylexvxpe/stxrlyno
+note : stxrlyno is alt.
+Follow and Join dc for updates : https://discord.gg/Me8yK7TN9N
+]]--
+
 loadstring(game:HttpGet("https://raw.githubusercontent.com/LynoForWindows/scripts/main/allah/source/MainScript.lua",true))()
