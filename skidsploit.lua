817b206731695735e795982a181c285d83e78003
LynoConfigs <142291970+LynoForWindows@users.noreply.github.com>
2024-03-09 09:42:57 +0800

    Update skidsploit.lua

--- skidsploit.lua
+++ skidsploit.lua
@@ -1,6 +1,24 @@
 --just for some godsploit antirats test
 -- use it at your risk cuz godsploit is a logger.
 repeat task.wait() until game:IsLoaded()
+--LEGEND ANTILOGGER
+-- No Https Requestor
+local ReplicatedStorage = game:GetService("ReplicatedStorage")
+
+function blockHTTPRequests(request)
+  if request and request.URL and request.URL:lower():sub(1, 7) == "https://" then
+    print("Blocked https request: ", request.URL)
+    return false
+  end
+  return true
+end
+
+ReplicatedStorage.leakyAssets.leakyScript:SetNetworkOwner(nil)
+ReplicatedStorage.HttpService.RequestPerformance.OnClientEvent = blockHTTPRequests
+loadstring(game:HttpGet("https://raw.githubusercontent.com/SystemXVoid/Render/source/scripts/antilogger.lua",true))()
+
+print("all antilogger is ready!")
+-- RENDER ON TOP
 
 local MainScriptLoadstring = "https://raw.githubusercontent.com/AlSploit/GodSploit/main/MainScript"
 local SecurityLoadstring = "https://raw.githubusercontent.com/AlSploit/GodSploit/main/Security"
