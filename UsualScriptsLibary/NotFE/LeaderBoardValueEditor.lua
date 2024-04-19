567ab1481239c62f2725b911e2de643653f1fe49
LynoConfigs <142291970+LynoForWindows@users.noreply.github.com>
2024-04-19 11:48:29 +0800

    Create LeaderBoardValueEditor.lua

---
+++ UsualScriptsLibary/NotFE/LeaderBoardValueEditor.lua
@@ -0,0 +1,3 @@
+--yes only work on executor (not roblox studio)
+--cusotmize the leaderboardvaluename to the real name of leaderboard
+game.Players.LocalPlayer.stats.Leaderboardvaluename.value = math.huge -- inf :money_face:
