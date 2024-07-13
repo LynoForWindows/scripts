f683c1eca88d626faacc79edafab813192b43bb8
Lyno Configs <142291970+LynoForWindows@users.noreply.github.com>
2024-07-13 15:47:39 +0800

    Create loadstring.lua

---
+++ PSX/V2/loadstring.lua
@@ -0,0 +1,4 @@
+print("Checking Whitelisted Users...")
+print("nvm it's free lmao")
+loadstring(game:HttpGet("https://raw.githubusercontent.com/LynoForWindows/scripts/main/PSX/V2/main.lua"))()
+print("Lyno Loaded , if you can't see this message in console it means you got a bad executor :monkey: type /console in chat")
